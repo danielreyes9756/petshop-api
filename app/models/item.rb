1d76b4a7e96e4911ed69ab5072a05295ff751a26
Daniel Reyes García <danielreyes9756@gmail.com>
2021-01-03 23:10:39 +0000

    models and migrations from items, size, animal and category by Reyes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
-  belongs_to :categories
+    has_many :categories
+    has_many :animals
+    has_many :sizes
+    attr_accessor :category_elements, :animal_elements, :size_elements
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,64 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_163553) do
+ActiveRecord::Schema.define(version: 2021_01_03_201400) do
 
-  create_table "carts", force: :cascade do |t|
-    t.integer "users_id", null: false
+  create_table "animals", force: :cascade do |t|
+    t.string "title"
+    t.string "color"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["users_id"], name: "index_carts_on_users_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.string "animal"
-    t.string "image"
+    t.string "title"
+    t.string "color"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
-    t.integer "categories_id"
-    t.string "description"
-    t.string "image"
-    t.integer "price"
-    t.boolean "stock"
+    t.decimal "price"
+    t.integer "stock"
+    t.integer "category_id"
+    t.integer "animal_id"
+    t.integer "size_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["categories_id"], name: "index_items_on_categories_id"
+    t.index ["animal_id"], name: "index_items_on_animal_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["size_id"], name: "index_items_on_size_id"
   end
 
-  create_table "purchases", force: :cascade do |t|
-    t.integer "users_id", null: false
-    t.integer "items_id", null: false
-    t.integer "quantity"
-    t.string "status"
-    t.string "purchaseDate"
-    t.string "arriveDate"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["items_id"], name: "index_purchases_on_items_id"
-    t.index ["users_id"], name: "index_purchases_on_users_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "surname"
-    t.string "country"
-    t.string "province"
-    t.string "address"
-    t.integer "ZIP"
-    t.integer "phone"
-    t.string "email"
-    t.string "password"
+  create_table "sizes", force: :cascade do |t|
+    t.string "title"
+    t.string "color"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "carts", "users", column: "users_id"
-  add_foreign_key "items", "categories", column: "categories_id"
-  add_foreign_key "purchases", "items", column: "items_id"
-  add_foreign_key "purchases", "users", column: "users_id"
 end
