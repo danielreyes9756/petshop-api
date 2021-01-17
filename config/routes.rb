5766ce35da8b9fc24124545bd6c71b3dd72af620
Daniel Reyes García <danielreyes9756@gmail.com>
2021-01-17 18:24:30 +0000

    last version

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
   resources :items
   resources :categories
   
-  
   get "/get_professionals", to: "fake_professionals#json_url"
   get "/get_categories", to: "categories#json_url"
   get "/get_animals", to: "animals#json_url"
   get "/get_items", to: "items#json_url"
   get "/get_sizes", to: "sizes#json_url"
+  get "/get_users", to: "user_own#json_url"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
